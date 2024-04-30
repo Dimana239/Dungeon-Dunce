3a06ee25cbbea5aae7f389df5c87b5647bef28a3
Calcium <callum.roberts9799@gmail.com>
2024-04-30 15:28:24 -0500

    Maybe Fixing Packaging?
    Bug where play button on main menu doesn't take you to the first level in the .exe but it does work in the project.

---
+++ Source/Game2Start/EmptyCClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmptyCClass.h"
+
+EmptyCClass::EmptyCClass()
+{
+}
+
+EmptyCClass::~EmptyCClass()
+{
+}
